12d251b77dcb3f158cf9797fcec2686d2fb98db9
Laurynas <laurynas@ginkus.lt>
2017-04-09 15:51:26 +0300

    Lesson 2 - string manipulation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Laurynas G.");
+
+var awesomeThoughts = "My name is Laurynas and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+//$("#main").append(funThoughts);
+
+
+var email = "laurynas@ginkus.lt";
+var newEmail = "laurynas.ginkus@gmail.com";
+
+var name = "Laurynas";
+var role = "Analyst";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["awesones", "coolness", "JS"];
+
+$("#header").append(skills);
